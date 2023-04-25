dee7b222b827ee2523eaa98651b2b5f53bec3339
johannesrave <johannes.rave@gmail.com>
2023-04-25 18:36:04 +0200

    introduced ConsoleCaptor

---
+++ src/test/kotlin/com/adaptionsoft/games/trivia/GoldenMasterTest.kt
@@ -0,0 +1,86 @@
+package com.adaptionsoft.games.trivia
+
+import com.adaptionsoft.games.trivia.runner.main
+import org.junit.jupiter.api.Disabled
+import java.io.File
+import java.io.PrintStream
+import kotlin.test.Test
+import kotlin.test.assertNull
+
+class GoldenMasterTest {
+    private val numberOfMasters = 10
+
+    @Test
+    @Disabled("Golden Masters should be created once and then be left alone.")
+    fun `writes goldenMaster to file`() {
+        repeat(numberOfMasters) {
+            val masterName = "testData/${it.toString().padStart(2, '0')}_goldenMaster.txt"
+            val masterFile = PrintStream(masterName)
+
+            System.setOut(masterFile)
+
+            main(arrayOf(it.toString()))
+        }
+    }
+
+    @Test
+    fun `compares output to goldenMaster`() {
+
+        val mismatches = mutableListOf<GoldenMasterMismatch>()
+
+        repeat(numberOfMasters) { randomSeed ->
+
+            val masterName = "testData/${randomSeed.toString().padStart(2, '0')}_goldenMaster.txt"
+            val testResultName = "testData/${randomSeed.toString().padStart(2, '0')}_testResult.txt"
+
+            ConsoleCaptor(testResultName).use {
+                main(arrayOf(randomSeed.toString()))
+            }
+
+            val masterLines = File(masterName).readLines()
+            val testResultLines = File(testResultName).readLines()
+
+            masterLines.forEachIndexed { lineIndex, _ ->
+                if (masterLines[lineIndex] == testResultLines[lineIndex]) return@forEachIndexed
+
+                mismatches.add(
+                    GoldenMasterMismatch(
+                        randomSeed,
+                        lineIndex,
+                        masterLines[lineIndex],
+                        testResultLines[lineIndex]
+                    )
+                )
+            }
+        }
+
+        mismatches.forEach { (fileIndex, lineIndex, masterLine, mismatchedLine) ->
+            assertNull(
+                Pair(fileIndex, lineIndex),
+                "\n" +
+                        "There is a mismatch in GoldenMaster $fileIndex on line $lineIndex:\n" +
+                        "MASTER: $masterLine\n" +
+                        "RESULT: $mismatchedLine\n"
+            )
+        }
+    }
+
+    data class GoldenMasterMismatch(
+        val fileIndex: Int,
+        val lineIndex: Int,
+        val masterLine: String,
+        val mismatchedLine: String
+    )
+
+    class ConsoleCaptor(fileName: String) : AutoCloseable {
+        private var console: PrintStream? = System.out
+
+        init {
+            System.setOut(PrintStream(fileName))
+        }
+
+        override fun close() {
+            System.setOut(console)
+        }
+    }
+}
