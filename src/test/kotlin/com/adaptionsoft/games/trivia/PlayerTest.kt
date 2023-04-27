9467eb7bcb269be2777e0d396605a0cba8afb7b2
johannesrave <johannes.rave@gmail.com>
2023-04-27 13:23:49 +0200

    added TurnSummaryTest

--- src/test/kotlin/com/adaptionsoft/games/trivia/PlayerTest.kt
+++ src/test/kotlin/com/adaptionsoft/games/trivia/PlayerTest.kt
@@ -9,6 +9,7 @@ class PlayerTest {
     @BeforeTest
     fun setup() {
         player = Player("Test Player")
+        Category.resetAllQuestions()
     }
     @Test
     fun `takeTurn should update player's coins if answer is correct`() {
@@ -45,5 +46,77 @@ class PlayerTest {
         player.inPenaltyBox = true
         player.takeTurn(2, 7)
         assertEquals(0, player.coins)
+    }
+    @Test
+    fun `takeTurn should return correct TurnSummary if player stays in the penalty box`() {
+        player.inPenaltyBox = true
+        val turnSummary = player.takeTurn(4, 6)
+        assertEquals(player, turnSummary.player)
+        assertTrue(turnSummary.answerIsCorrect)
+        assertFalse(turnSummary.leavesPenaltyBox)
+        assertTrue(turnSummary.staysInPenaltyBox)
+        assertEquals(4, turnSummary.placeRoll)
+    }
+}
+
+
+class TurnSummaryTest {
+    @BeforeTest
+    fun setup() {
+        Category.resetAllQuestions()
+    }
+    @Test
+    fun `toString should return correct string when player stays in penalty box`() {
+        val player = Player("Test Player", inPenaltyBox = true)
+        val summary = TurnSummary(player,
+            answerIsCorrect = true,
+            leavesPenaltyBox = false,
+            staysInPenaltyBox = true,
+            placeRoll = 4
+        )
+        val expected = "Test Player is the current player\n" +
+                "They have rolled a 4\n" +
+                "Test Player is not getting out of the penalty box"
+        assertEquals(expected, summary.toString())
+    }
+
+    @Test
+    fun `toString should return correct string when player leaves penalty box and answer is correct`() {
+        val player = Player("Test Player", inPenaltyBox = true, coins = 1, place = 3)
+        val summary = TurnSummary(player,
+            answerIsCorrect = true,
+            leavesPenaltyBox = true,
+            staysInPenaltyBox = false,
+            placeRoll = 5
+        )
+        val expected = "Test Player is the current player\n" +
+                "They have rolled a 5\n" +
+                "Test Player is getting out of the penalty box\n" +
+                "Test Player's new location is 3\n" +
+                "The category is Rock\n" +
+                "Rock Question 0\n" +
+                "Answer was correct!!!!\n" +
+                "Test Player now has 1 Gold Coins."
+        assertEquals(expected, summary.toString())
+    }
+
+    @Test
+    fun `toString should return correct string when player leaves penalty box and answer is incorrect`() {
+        val player = Player("Test Player", inPenaltyBox = true, place = 7)
+        val summary = TurnSummary(player,
+            answerIsCorrect = false,
+            leavesPenaltyBox = true,
+            staysInPenaltyBox = false,
+            placeRoll = 4
+        )
+        val expected = "Test Player is the current player\n" +
+                "They have rolled a 4\n" +
+                "Test Player is getting out of the penalty box\n" +
+                "Test Player's new location is 7\n" +
+                "The category is Rock\n" +
+                "Rock Question 0\n" +
+                "Question was incorrectly answered\n" +
+                "Test Player was sent to the penalty box"
+        assertEquals(expected, summary.toString())
     }
 }
